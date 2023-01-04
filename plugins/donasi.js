389b0c899773a927849597c8a95d571a06e28546
Lans turu ygy <111067376+Lans-ofc@users.noreply.github.com>
2023-01-04 23:46:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
-    let img2 = `https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg`
+    let img2 = `https://telegra.ph/file/f2d8864b2ee539cce0e8b.jpg`
     return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/l4c1f2r",
+                mediaUrl: "https://instagram.com/lans.xys",
                 mediaType: "VIDEO",
-                title: 'Donasi To PutraModz',
+                title: 'Donasi To LansModz',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
